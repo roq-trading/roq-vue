f4efe609a72ce1462c95a8c1aebca8fa3d1319d6
thraneh <thraneh@gmail.com>
2022-11-15 14:01:29 +0100

    update

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,14 @@ import dayjs from "dayjs"
 export default {
 	data() {
 		return { 
-			data: {},
 			gateways: [ "deribit", "ftx" ],
 			gateway: null,
 			exchanges: [],
 			exchange: null,
 			symbols: [],
-			symbol: null
+			symbol: null,
+			reference_data: {},
+			top_of_book: {},
 		}
 	},
 	methods: { 
@@ -35,20 +36,25 @@ export default {
 				.get(`http://localhost:1234/top_of_book/${this.exchange}`)
 				.then(response => (this.symbols = response.data))
 		},
+		fetch_reference_data() { 
+			axios
+				.get(`http://localhost:1234/reference_data/${this.exchange}/${this.symbol}`)
+				.then(response => (this.reference_data = response.data))
+		},
 		fetch_top_of_book() { 
 			axios
 				.get(`http://localhost:1234/top_of_book/${this.exchange}/${this.symbol}`)
-				.then(response => {this.data = response.data;
-							      setTimeout(() => this.fetch_top_of_book(), 1000)
+				.then(response => {this.top_of_book = response.data;
+							      setTimeout(() => this.fetch_top_of_book(), 5000)
 						  }) 
 		},
 	},
 	watch: {
 		gateway(a,b) { this.fetch_exchanges() },
-		exchange(a,b) { this.fetch_symbols() }
+		exchange(a,b) { this.fetch_symbols() },
+		symbol(a,b) { this.fetch_reference_data(); this.fetch_top_of_book(); },
 	},
 	created() {
-		this.fetch_top_of_book()
 	},
 	mounted() {
 	}
@@ -76,12 +82,13 @@ export default {
 	</select>
 	Symbol: {{ symbol }}
 	</div>
-	<div v-if="Object.keys(data).length>0">
-	<div>{{ data.exchange }} / {{ data.symbol }}</div>
-	<div>{{ data.layer.bid_price }} x {{ data.layer.ask_price }}</div>
-	<div>{{ format_time(data.exchange_time_utc) }}</div>
-	<div>{{ format_date(data.exchange_time_utc) }}</div>
+	<div v-if="Object.keys(top_of_book).length>0">
+	<div>{{ top_of_book.exchange }} / {{ top_of_book.symbol }}</div>
+	<div>{{ top_of_book.layer.bid_price }} x {{ top_of_book.layer.ask_price }}</div>
+	<div>{{ format_time(top_of_book.exchange_time_utc) }}</div>
+	<div>{{ format_date(top_of_book.exchange_time_utc) }}</div>
 	</div>
+	<div v-if="Object.keys(reference_data).length>0">{{ reference_data }}</div>
 </template>
 
 <style scoped>
