4b803bc673696390199ed3a7025a20472b1cddc1
thraneh <thraneh@gmail.com>
2022-11-20 13:55:23 +0100

    adding conda build

---
+++ conda/build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo -e "\033[1;34m--- ENV ---\033[0m"
+
+env | sort
+
+echo -e "\033[1;34m--- BUILD ---\033[0m"
+
+npm run build
+
+echo -e "\033[1;34m--- DONE ---\033[0m"
