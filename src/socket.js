730de3b08e281b41165fe86441099b794a7b31cd
thraneh <thraneh@gmail.com>
2024-12-12 09:43:03 +0100

    update

--- src/socket.js
+++ src/socket.js
@@ -1,4 +1,4 @@
-const socket = new WebSocket('ws://localhost:2345/');
+const socket = new WebSocket('ws://localhost:2345/roq/');
 
 socket.onerror = function(event) {
   console.log('error: ', event);
