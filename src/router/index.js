9ae18d767a1939e02e79f50fb1f78e6716ba334a
thraneh <thraneh@gmail.com>
2024-12-14 13:40:19 +0100

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,61 +1,49 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {createRouter, createWebHistory} from 'vue-router';
 
-import HomeView from "../views/HomeView.vue";
+import HomeView from '../views/HomeView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
-      name: "home",
+      path: '/',
+      name: 'home',
       component: HomeView,
     },
     {
-      path: "/status",
-      name: "status",
-      component: () => import("../views/StatusView.vue"),
+      path: '/public',
+      name: 'public',
+      component: () => import('../views/PublicView.vue'),
       props: false,
     },
     {
-      path: "/status/:gateway",
-      name: "status_by_gateway",
-      component: () => import("../views/StatusView.vue"),
+      path: '/public/:gateway',
+      name: 'public_by_gateway',
+      component: () => import('../views/PublicView.vue'),
       props: true,
     },
     {
-      path: "/public",
-      name: "public",
-      component: () => import("../views/PublicView.vue"),
+      path: '/private',
+      name: 'private',
+      component: () => import('../views/PrivateView.vue'),
       props: false,
     },
     {
-      path: "/public/:gateway",
-      name: "public_by_gateway",
-      component: () => import("../views/PublicView.vue"),
+      path: '/private/:gateway',
+      name: 'private_by_gateway',
+      component: () => import('../views/PrivateView.vue'),
       props: true,
     },
     {
-      path: "/private",
-      name: "private",
-      component: () => import("../views/PrivateView.vue"),
+      path: '/strategy',
+      name: 'strategy',
+      component: () => import('../views/StrategyView.vue'),
       props: false,
     },
     {
-      path: "/private/:gateway",
-      name: "private_by_gateway",
-      component: () => import("../views/PrivateView.vue"),
-      props: true,
-    },
-    {
-      path: "/strategy",
-      name: "strategy",
-      component: () => import("../views/StrategyView.vue"),
-      props: false,
-    },
-    {
-      path: "/strategy/:gateway",
-      name: "strategy_by_gateway",
-      component: () => import("../views/StrategyView.vue"),
+      path: '/strategy/:gateway',
+      name: 'strategy_by_gateway',
+      component: () => import('../views/StrategyView.vue'),
       props: true,
     },
   ],
